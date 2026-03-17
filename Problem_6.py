f0d4721aeeca5d2e6c940df02459b7eeeb83a95e
hitesh61615-sys <hitesh61615@gmail.com>
2026-03-17 21:14:29 +0530

    Problem_6.py

--- Problem_6.py
+++ Problem_6.py
@@ -1,3 +1,3 @@
 # Q6. Write a python program to calculate the square of a number entered by the user.
 a = int(input("Enter the number:"))
-print("Square of the number entered is:",(a*a))
+print("Square of the number entered is:",(a**2))
